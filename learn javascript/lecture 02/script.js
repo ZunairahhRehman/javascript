77ef1146c89362c0c608133b8854e4e30ce6649e
zunaira rehman <zunairarehman794@gmail.com>
2025-04-23 13:31:29 -0500

    Add files via upload

---
+++ learn javascript/lecture 02/script.js
@@ -0,0 +1,138 @@
+// Arthimetic operator
+// let a = 5;
+// let b = 2;
+
+// console.log("a+b =", a+b);
+// console.log("a-b =", a-b);
+// console.log("a*b =", a*b);
+// console.log("a/b =", a/b);
+// console.log("a%b =", a%b);
+// console.log("a**b =", a**b);
+
+
+// Unary Operator
+
+// console.log("a++ =", a++);
+// console.log("a =", a);
+// console.log("a-- =", a--);
+// console.log("a =", a);
+
+// console.log("++a =", ++a);
+// console.log("--a =", --a);
+
+// Assignment operatore
+
+// let a = 5;
+// let b = 2;
+// // a += 4; // a = a+4
+// // a -= 4; // a = a-4
+// // a -= 4; // a = a-4
+// // a *= 4; // a = a*4
+// // a %= 4; // a = a%4
+// a **= 4; // a = a**4
+
+// console.log("a =", a);
+
+// comparison operator
+
+// let a = 5;
+// let b = 3;
+
+// console.log("a==b", a==b);
+// console.log("a===b", a===b);
+// console.log("a!==b", a!==b);
+// console.log("a>b", a>b);
+// console.log("a>=b", a>=b);
+// console.log("a<b", a<b);
+
+// logical operator
+// let a = 6;
+// let b = 5;
+// let cond1=a>b;
+// let cond2=a===6;
+
+
+// // console.log("cond1&&cond2", cond1&&cond2);
+// console.log("cond1||cond2", cond1||cond2);
+
+// coditional statements
+
+// let age =18;
+// if (age <18){
+// console.log("you cannot vote");
+// }
+// else{
+//     console.log("you can vote");
+// }
+ 
+
+//  let mode="dark";
+//  let color;
+
+// if (mode==="dark"){
+//     color = "black";
+//     }
+//     else {
+//         color = "white";
+//     }
+// console.log(color);
+
+// conditional statements
+// odd or event
+// let num=15;
+// if(num % 2===0){
+//     console.log(num,"is even");
+// } else{
+//     console.log(num,"is odd")
+// }
+
+// let mode="brown";
+//  let color;
+
+// if (mode==="dark"){
+//     color = "black";
+//     }
+//     else if(mode==="blue") {
+//         color = "blue";
+//     } else if( mode==="pink"){
+//         color="pink";
+//     }else{
+//         color="white"
+//     }
+// console.log(color);
+
+// let age=25;
+// let result = age>=18?"adult":"not adult";
+// console.log(result);
+     
+// let num = prompt("enter a number:");
+// if(num% 5===0){
+//     console.log(num,"is a multiple of 5");
+// }else{
+//     console.log(num,"is not a multiple of 5");
+
+// }
+
+let score =75;
+let grade;
+if(score >= 90&& score<= 100){
+    grade = "A";
+} else if(score >= 70&& score<= 89){
+    grade = "B";
+}else if(score >= 60&& score<= 69){
+    grade = "C";
+}else if(score >= 50&& score<= 59){
+    grade = "D";
+} else if(score >= 0&& score<= 49){
+    grade = "f";
+}
+console.log("according to your scores was:", grade);
+
+
+
+
+
+
+
+
+
